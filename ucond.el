fc4a8ab1b5962ffb9f0f8b27a2f2ccb833703797
hawnzug <hawnzug@gmail.com>
2025-06-29 20:07:24 -0500

    * ucond.el, .gitignore, LICENSE: Init

---
+++ ucond.el
@@ -0,0 +1,20 @@
+;;; ucond.el --- Ultimate Conditional Syntax with let-else  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025  Zhuyang Wang
+
+;; Author: Zhuyang Wang <hawnzug@gmail.com>
+;; Created: 2025-06-29
+;; Keywords: macros
+;; Version: 0.1.0
+
+;;; Commentary:
+
+;; Combine the Ultimate Conditional Syntax from MLscript
+;; (see https://doi.org/10.1145/3689746)
+;; with let-else, using pcase.
+
+;;; Code:
+
+(provide 'ucond)
+
+;;; ucond.el ends here
